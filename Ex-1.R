ec97bb5752e0949e60d4956d2d758bf93ebb8a23
11249m016-stack <11249m016@kanchiuniv.ac.in>
2026-04-23 17:17:50 +0530

    .

--- Ex-1.R
+++ Ex-1.R
@@ -1,17 +1,17 @@
 1.1
 Student Information System
 R Program:
-roll_no <- 101 # Integer
-name <- "Anitha" # Character
-cgpa <- 8.45 # Numeric
+roll_no <- 102 # Integer
+name <- "Anika" # Character
+cgpa <- 9.00 # Numeric
 pass <- TRUE # Logical
 
 1.2
 Employee Salary Record
 R program:
-emp_id <- 5001
-salary <- 45000.75
-department <- "IT"
+emp_id <- 5009
+salary <- 50000.00
+department <- "CSE"
 is_permanent <- TRUE
 emp_id
 salary
@@ -21,9 +21,9 @@ is_permanent
 1.3
 Online Shopping Cart
 R program:
-product_name <- "Laptop"
-price <- 55000
-stock_available <- 25
+product_name <- "MAC BOOK"
+price <- 150000
+stock_available <- 30
 discount_eligible <- FALSE
 product_name
 price
@@ -33,8 +33,8 @@ discount_eligible
 1.4
 Weather Monitoring System
 R program:
-city <- "Chennai"
-temperature <- 36.5
+city <- "ONGOLE"
+temperature <- 37.9
 rain <- FALSE
 wind_speed <- 12.8
 city
@@ -45,9 +45,9 @@ wind_speed
 1.5
 Examination Result Processing
 R program:
-subject <- "Mathematics"
+subject <- "SOCIAL"
 total_marks <- 100
-marks_obtained <- 78
+marks_obtained <- 90
 result <- marks_obtained >= 40
 subject
 total_marks
@@ -57,9 +57,9 @@ result
 1.6
 Banking Application
 R program:
-account_no <- 123456789
-holder_name <- "Ravi Kumar"
-balance <- 25000.50
+account_no <- 998858549
+holder_name <- "SATHEESH"
+balance <- 35000.50
 account_active <- TRUE
 account_no
 holder_name
@@ -69,9 +69,9 @@ account_active
 1.7
 Movie Rating System
 R program:
-movie_title <- "Inception"
-rating <- 8.8
-reviews <- 1200
+movie_title <- "USTAAD BHAGATH SINGH"
+rating <- 9.9
+reviews <- 1100000
 available <- TRUE
 movie_title
 rating
@@ -81,8 +81,8 @@ available
 1.8
 Traﬃc Monitoring System
 R program:
-vehicle_count <- 350
-location <- "Highway Road"
+vehicle_count <- 420
+location <- "NH40 HIGHWAY"
 avg_speed <- 45.6
 congested <- TRUE
 vehicle_count
